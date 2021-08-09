5820b6a3d31bd330c215c816eefcdf6ec507e434
JOCECODE <ulah57@gmail.com>
2021-08-09 15:04:53 -0700

    create props;

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import MovieRows from "./components/MovieRows";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There!</h1>
+      <MovieRows title="Netflix Originals" />
+      <MovieRows title="Trending" />
+      <MovieRows title="RomanticComedies" />
     </div>
   );
 }
