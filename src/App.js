452d3a2528890684cc8a04bb32243e8587d5b166
JOCECODE <ulah57@gmail.com>
2021-08-09 15:23:51 -0700

    create axios call successfully;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import MovieRows from "./components/MovieRows";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello There!</h1>
-      <MovieRows title="Netflix Originals" />
-      <MovieRows title="Trending" />
-      <MovieRows title="RomanticComedies" />
+      <MovieRows title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <MovieRows title="Trending" fetchUrl={requests.fetchTrending}/>
+      <MovieRows title="RomanticComedies" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }
--- src/components/MovieRows.js
+++ src/components/MovieRows.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "../axios"; 
 
-export default function MovieRows({ title }) {
+export default function MovieRows({ title, fetchUrl }) {
+ 
+ const [movies, setMovies] = useState([]);
+ useEffect(() => {
+
+  async function fetchData() {
+    const req = await axios.get(fetchUrl);
+    console.log(req);
+    console.table(req);
+    return req;
+  }
+  fetchData();
+ }, []);
+ 
  return (
   <>
    <h2>{ title }</h2>
