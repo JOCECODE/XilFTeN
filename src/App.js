8aa910d28d72b64dd9fbfe63f060a715963fa3b2
JOCECODE <ulah57@gmail.com>
2021-08-18 19:16:27 -0700

    add profile picture;

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <h1 className="xilften">XiLFTeN</h1>
       <Navbar/>
       <Banner/>
       <MovieRows 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import ProfilePicture from "../pictures/dogProfile.jpg";
 import "./Navbar.css";
 
 function Navbar() {
     return (
         <>
-            <h1 className="testerC">Hello World</h1>
+            <div className="nav">
+                 <h1 className="xilften">XiLFTeN</h1>
+
+                 <img
+                 className="avatar"
+                 src={ProfilePicture}
+                 />
+            </div>
         </>
     )
 }
