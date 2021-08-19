4bab9d4df1eb7344cb0f3ecf7c0ab9439ddda3fb
JOCECODE <ulah57@gmail.com>
2021-08-18 19:56:45 -0700

    finsh navbar and css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfilePicture from "../pictures/dogProfile.jpg";
 import "./Navbar.css";
 
 function Navbar() {
+    const [show, handleShow] = useState(false);
+   
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                handleShow(true);
+            }
+            else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
+
     return (
         <>
-            <div className="nav">
+            <div className={`nav ${show && "nav_black"}`}>
                  <h1 className="xilften">XiLFTeN</h1>
 
                  <img
                  className="avatar"
                  src={ProfilePicture}
+                 alt="Dog Avatar"
                  />
             </div>
         </>
